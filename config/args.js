7abf2e3917481398e60f0183ec88b5da49bf8fe9
alberto.cavazzoni <albecava98@gmail.com>
2023-05-13 16:18:17 +0200

    crawling/ first commit

--- config/args.js
+++ config/args.js
@@ -3,9 +3,9 @@ import { hideBin } from 'yargs/helpers';
 
 export default yargs(hideBin(process.argv))
     .scriptName('binge-watching-crawling')
-    .usage('$0 \n', 'node index.js [options]')
+    .usage('$0', 'node index.js [options]')
     .version('1.0.0')
-    .locale('it')
+    .locale('en')
     .options({
         ids: {
             requireArg: true,
@@ -21,10 +21,16 @@ export default yargs(hideBin(process.argv))
             type: 'boolean',
             describe: 'Crawla il catalogo delle serie TV',
         },
+        crawl: {
+            type: 'boolean',
+            describe: 'Crawla tutte le pagina delle serie TV',
+        },
     })
+    .conflicts({ ids: ['glues', 'catalog', 'crawl'], glues: ['catalog', 'crawl'], catalog: ['crawl'] })
     .example([
         ['node index.js --ids=2', '-> Crawla la serie TV con id === 2'],
         ['node index.js --catalog', '-> Crawla il catalogo delle serie TV'],
     ])
+    .strict()
     .help()
     .parse();
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import args from './config/args.js';
+import { gotScraping } from 'got-scraping';
+import * as cheerio from 'cheerio';
 
 console.log(args);
+
+async function scrape() {
+    const response = await gotScraping('https://www.comingsoon.it/serietv/ricerca/');
+
+    const $ = cheerio.load(response.body);
+
+    const series = [];
+
+    $('.cards.serie-tv').each((i, el) => {
+        const title = $(el).find('.titolo').text().trim();
+        const country = $(el).find('.meta .p-small:contains("Nazionalità") span').text().trim();
+        const genre = $(el).find('.meta .p-small:contains("Genere") span').text().trim();
+        const year = $(el).find('.meta .p-small:contains("Anno") span').text().trim();
+        series.push({ title, country, genre, year });
+    });
+
+    console.table(series);
+}
+
+scrape();
